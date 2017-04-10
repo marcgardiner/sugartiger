37f00ee00b4fee69e3547839d60d34dd13acf6e6
service <servicemanager3386@yandex.com>
2017-04-10 10:57:03 -0400

    modified by dev admin

--- sugartiger/stapp/admin.py
+++ sugartiger/stapp/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Publisher, Author, Book
+
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)
 
 # Register your models here.
